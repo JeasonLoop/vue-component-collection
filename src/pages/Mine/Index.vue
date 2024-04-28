f103b037b062973ea16553de74633b29c72fa985
fansj <fsj77524@163.com>
2024-04-28 11:17:39 +0800

    feat: 我的页面新增

--- src/pages/Mine/Index.vue
+++ src/pages/Mine/Index.vue
@@ -1,13 +1,36 @@
 <script setup>
+import avatar from '../../assets/avatar.jpg'
 
 </script>
 
 <template lang="">
-  <div>
-    this is mine
+  <div class='user_info'>
+    <t-avatar
+      class="avatar"
+      :image="avatar"
+      size='large'
+      style='--td-avatar-large-width:100px'
+    />
+    <div class='username'>
+      JeasonLoop
+    </div>
   </div>
-</template>
+</template> 
 
-<style lang="">
+<style lang="css">
+  .user_info {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+  }
 
+  .username {
+    margin: calc(30 / 32 * 1rem) 0;
+    color: #333;
+    font-size: calc(33 / 32 * 1rem);
+    font-weight: 600;
+  }
 </style>
