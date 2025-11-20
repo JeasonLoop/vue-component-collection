7051c5754c38df8ca130aca3f0f0a929408de3e1
fansj <fsj77524@163.com>
2025-11-20 17:41:24 +0800

    fix: change base url

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/vue-component-collection/',
   plugins: [
     vue(),
     vueJsx(),
