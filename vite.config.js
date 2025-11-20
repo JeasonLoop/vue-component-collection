c9e7609a3a7c90b0dbb2dd6ae7df908409db38b6
fansj <fsj77524@163.com>
2025-11-20 17:59:17 +0800

    fix: deploy flow fix

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ import vueJsx from '@vitejs/plugin-vue-jsx'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './',
+  base: '/vue-component-collection/',
   plugins: [
     vue(),
     vueJsx(),
